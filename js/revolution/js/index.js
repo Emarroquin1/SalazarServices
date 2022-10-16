bbc14f5804dd5c4b4a37f1e58b3cbb6742698b64
Emarroquin1 <82725260+Emarroquin1@users.noreply.github.com>
2022-10-15 18:53:54 -0600

    prueva

--- js/revolution/js/index.js
+++ js/revolution/js/index.js
@@ -12,14 +12,14 @@
         
         document.getElementById('capa1').style.backgroundColor ='rgb(60, 179, 113,0.7)';
         document.getElementById('a1').style.top='0%';
-        document.getElementById('a1').style.color='black';
+        document.getElementById('a1').style.color='rgb(9, 255, 140)';
         document.getElementById('p1').style.visibility='visible';
            validador=2;
         }else if(validador==2){
         document.getElementById('capa1').style.backgroundColor ='transparent';
         document.getElementById('a1').style.top='45%';
         document.getElementById('p1').style.visibility='hidden'; 
-        document.getElementById('a1').style.color='#09ff8c';
+        document.getElementById('a1').style.color='white';
         document.getElementById('capa1').style.boxShadow='0px 0px 2px';
         validador=1;
         
@@ -40,14 +40,14 @@
     document.getElementById('capa2').style.boxShadow='0px 15px 25px';
     document.getElementById('capa2').style.backgroundColor ='rgb(60, 179, 113,0.7)';
     document.getElementById('a2').style.top='0%';
-    document.getElementById('a2').style.color='black';
+    document.getElementById('a2').style.color='rgb(9, 255, 140)';
     document.getElementById('p2').style.visibility='visible';
        validador2=2;
     }else if(validador2==2){
     document.getElementById('capa2').style.backgroundColor ='transparent';
     document.getElementById('a2').style.top='45%';
     document.getElementById('p2').style.visibility='hidden'; 
-    document.getElementById('a2').style.color='#09ff8c';
+    document.getElementById('a2').style.color='white';
     document.getElementById('capa2').style.boxShadow='0px 0px 2px';
     validador2=1;
     
@@ -63,14 +63,14 @@
     document.getElementById('capa3').style.boxShadow='0px 15px 25px';
     document.getElementById('capa3').style.backgroundColor ='rgb(60, 179, 113,0.7)';
     document.getElementById('a3').style.top='0%';
-    document.getElementById('a3').style.color='black';
+    document.getElementById('a3').style.color='rgb(9, 255, 140)';
     document.getElementById('p3').style.visibility='visible';
        validador3=2;
     }else if(validador3==2){
     document.getElementById('capa3').style.backgroundColor ='transparent';
     document.getElementById('a3').style.top='45%';
     document.getElementById('p3').style.visibility='hidden'; 
-    document.getElementById('a3').style.color='#09ff8c';
+    document.getElementById('a3').style.color='white';
     document.getElementById('capa3').style.boxShadow='0px 0px 2px';
     validador3=1;
     
@@ -86,14 +86,14 @@
        document.getElementById('capa4').style.boxShadow='0px 15px 25px';
     document.getElementById('capa4').style.backgroundColor ='rgb(60, 179, 113,0.7)';
     document.getElementById('a4').style.top='0%';
-    document.getElementById('a4').style.color='black';
+    document.getElementById('a4').style.color='rgb(9, 255, 140)';
     document.getElementById('p4').style.visibility='visible';
        validador4=2;
     }else if(validador4==2){
     document.getElementById('capa4').style.backgroundColor ='transparent';
     document.getElementById('a4').style.top='45%';
     document.getElementById('p4').style.visibility='hidden'; 
-    document.getElementById('a4').style.color='#09ff8c';
+    document.getElementById('a4').style.color='white';
     document.getElementById('capa4').style.boxShadow='0px 0px 2px';
     validador4=1;
     
